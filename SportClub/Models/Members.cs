9af932f802a79c2a29a6ce63d845abab80eb20c2
Jelena Buncic <jelena_buncic@one.pmf.uns.ac.rs>
2015-03-04 10:38:03 +0100

    Create Db

---
+++ SportClub/Models/Members.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportClub.Models
+{
+    public class Members
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public String Sport { get; set; }
+        public string Group { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
